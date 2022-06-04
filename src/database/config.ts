8c228eb4aef6c370892883cdada16ea4bd91b966
wolwer <ricardo@dtmm.com.br>
2022-06-04 00:03:38 -0300

    configuração heroku

--- src/database/config.ts
+++ src/database/config.ts
@@ -4,15 +4,21 @@ import { Feedbacks } from '../entities/feedbacks.entity';
 
 export const connection = createConnection({
     type: 'postgres',
-    host: 'localhost',
+    host: 'ec2-44-196-174-238.compute-1.amazonaws.com',
     port: 5432,
-    username: 'postgres',
-    password: '123',
-    database: 'usersNode',
+    username: 'zuuzgarsgeejdu',
+    password: '700ced2b27a2f2c056f2502645974d397ff3ab1c3f3550f980aebb165d999b89',
+    database: 'ddtl155b845btk',
     entities: [
         Usuarios,
         Feedbacks
     ],
     logging: true,
-    synchronize: true
+    synchronize: true,
+    ssl: true,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        }
+    }
 });
